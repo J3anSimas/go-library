d64565347fd5ddfe209f3d9eef2368daacc352e0
Jean Simas <jeansimas16@gmail.com>
2025-11-14 00:48:34 -0300

    Reading from env the right way

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,8 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
     imports: [UsersModule,
